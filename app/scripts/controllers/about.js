d393919d7707b94c345a1c950453e77683e3fadf
Marcel de Jesus Silva <silva.marcel@gmail.com>
2015-07-20 19:57:49 -0300

    Project created

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ionicOauthServiceApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ionicOauthServiceApp
+ */
+angular.module('ionicOauthServiceApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ionicOauthServiceApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ionicOauthServiceApp
+ */
+angular.module('ionicOauthServiceApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
